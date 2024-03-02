b6947f600fae4e7b09811e1f03d2ec70da1963de
Pratik Kale <ppvkale@gmail.com>
2024-03-02 16:37:54 +0530

    Tech stack

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,49 @@ import CardListSkeleton from "@/components/card-list-skeleton";
 import ParagraphSkeleton from "@/components/paragraph-skeleton";
 import Profile from "@/components/profile";
 import { cn, fadeIn } from "@/lib/utils";
+import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function Home() {
+  const images = [
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg",
+      alt: "JavaScript",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+      alt: "TypeScript",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+      alt: "Nextjs",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-plain.svg",
+      alt: "C++",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+      alt: "React",
+    },
+    {
+      src: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+      alt: "Tailwind CSS",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+      alt: "Firebase",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/googlecloud/googlecloud-original.svg",
+      alt: "Google Cloud",
+    },
+    {
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-plain.svg",
+      alt: "Docker",
+    },
+  ];
+
   return (
     <main className="flex flex-col gap-2 mt-6 ">
       <section className={cn(fadeIn, "animation-delay-200")}>
@@ -19,8 +59,19 @@ export default async function Home() {
           <span className="text-justify  mt-3">
             Pratik is a Developer and Cloud enthusiast. He is the organizer of GDG Cloud Pune. He was also the GDSC Lead for the year 2021-2022.
             Pratik enjoys writing technical blogs and articles to share his insights with the community. He is often found discussing Cloud Native
-            technologies, DevOps, and Cloud Computing. When he&apos;s not in a terminal, Pratik enjoys bike rides and photography.
+            technologies, DevOps, and Web. When he&apos;s not coding, he enjoys bike rides and photography.
           </span>
+        </Suspense>
+      </section>
+
+      <section className={cn(fadeIn, "animation-delay-400 ")}>
+        <Suspense fallback={<CardListSkeleton />}>
+          <h2 className="text-xl font-bold mt-2">Stuff I Know</h2>
+          <p className="flex flex-wrap gap-4 mt-4">
+            {images.map((image) => (
+              <Image key={image.alt} src={image.src} alt={image.alt} width={45} height={45} />
+            ))}
+          </p>
         </Suspense>
       </section>
     </main>
--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -4,7 +4,7 @@ export default async function bio() {
   return (
     <div className="mt-3">
       <p className="text-center">
-        Hi there 👋, I am <b>Pratik Kale</b> Developer and Community Evangelist <br /> Public Speaker & Organizer at{" "}
+        Hi there 👋, I am <b>Pratik Kale</b>, a Developer and Community Evangelist <br /> Public Speaker & Organizer at{" "}
         <Link className="underline decoration-mycolor hover:text-mycolor decoration-2" href="https://gdgcloudpune.com" target="_blank">
           GDG Cloud Pune
         </Link>

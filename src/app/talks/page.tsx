e52cb42db44046475f0a131e464c19cf7c886fd7
Pratik Kale <ppvkale@gmail.com>
2024-03-04 02:59:10 +0530

    Add introduction paragraph to Talks page

--- src/app/talks/page.tsx
+++ src/app/talks/page.tsx
@@ -55,6 +55,11 @@ export default async function Talks() {
     <main>
       <section className={cn(fadeIn, "animation-delay-200 mt-6 ")}>
         <h1 className="font-bold text-2xl mb-3">{data?.publication.staticPage.title}</h1>
+        <p className="mb-6 text-justify">
+          I love diving into the world of tech, and even more so, sharing what I discover. You&apos;ll often find me giving talks or running workshops
+          at community events – it&apos;s where I get to geek out with others who are just as curious. Let&apos;s explore the latest in tech and learn
+          something new together!
+        </p>
         <div className="flex flex-col items-center">
           <Mdx code={data?.publication.staticPage.content.markdown} />
         </div>
